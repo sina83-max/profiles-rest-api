ade662fdba7545f5140d92fc350773f4df362ab3
Sina <s.beignazary1383@gmail.com>
2025-02-23 17:07:19 +0330

    Add custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profile"""
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User most have an email address.")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a superuser with the given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return  user
+
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    # Replace the Default username field in the model with the email field
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name',]
+
+    def get_full_name(self):
+        """Retrieve the full name of the user"""
+        return self.name
+    def get_short_name(self):
+        """Retrieve the short name of the user"""
+        return self.name
+        # Because we merged the first and last name in our model, we can't seperate the long and short names!
+
+    def __str__(self):
+        """Return a string representation of our user model"""
+        return self.email
