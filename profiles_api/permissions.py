4f1ad98439baf644aff0d15affb8474310349573
Sina <s.beignazary1383@gmail.com>
2025-02-27 10:22:14 +0330

    Add user profile feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,3 +1,4 @@
+from django.template.context_processors import request
 from rest_framework import permissions
 
 class UpdateOwnProfile(permissions.BasePermission):
@@ -9,3 +10,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Check the user is trying to update their own status"""
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
+
+
